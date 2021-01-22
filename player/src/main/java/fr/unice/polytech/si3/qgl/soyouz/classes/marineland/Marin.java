9ca13242cc270d864faa8d33cd39e7904e911b41
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2021-01-22 12:08:19 +0100

    refactoring

--- player/src/main/java/fr/unice/polytech/si3/qgl/soyouz/classes/marineland/Marin.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/soyouz/classes/marineland/Marin.java
@@ -8,6 +8,8 @@ public class Marin {
     private int y;
     private String name;
 
+    public static final int maxMove = 5;
+
     public int getId() {
         return id;
     }
@@ -45,7 +47,7 @@ public class Marin {
      * @throws IllegalArgumentException
      */
     public Pair<Integer, Integer> moveRelative(int xDist, int yDist) throws IllegalArgumentException {
-        if (Math.abs(xDist) + Math.abs(yDist) > 5)
+        if (!isRelPosReachable(xDist, yDist))
             throw new IllegalArgumentException("Sailor must move 5 cells or lower");
         this.x += xDist;
         this.y += yDist;
@@ -65,7 +67,7 @@ public class Marin {
      * @throws IllegalArgumentException
      */
     public Pair<Integer, Integer> moveAbsolute(int xPos, int yPos) throws  IllegalArgumentException{
-        if(Math.abs(this.x - xPos) + Math.abs(this.y - yPos) > 5)
+        if(!this.isAbsPosReachable(xPos, yPos))
             throw new IllegalArgumentException("Sailor must move 5 cells or lower");
         this.x = xPos;
         this.y = yPos;
@@ -74,5 +76,25 @@ public class Marin {
 
     public Pair<Integer, Integer> moveAbsolute(Pair<Integer, Integer> pos) throws  IllegalArgumentException{
         return moveAbsolute(pos.getFirst(), pos.getSecond());
+    }
+
+    /**
+     *
+     * @param xDist distance on X axis
+     * @param yDist distance on X axis
+     * @return if the sailor can move the number of cells specified
+     */
+    public boolean isRelPosReachable(int xDist, int yDist){
+        return Math.abs(xDist) + Math.abs(yDist) <= maxMove;
+    }
+
+    /**
+     *
+     * @param xPos cell on X axis
+     * @param yPos cell on Y axis
+     * @return if the absolute position is reachable for this sailor
+     */
+    public boolean isAbsPosReachable(int xPos, int yPos){
+        return Math.abs(this.x - xPos) + Math.abs(this.y - yPos) <= maxMove;
     }
 }
