e448e7b59b37ff0c6e1cd43baa8c012c0995dc0c
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2021-01-24 13:42:15 +0100

    default impl for OnboardEntity

--- player/src/main/java/fr/unice/polytech/si3/qgl/soyouz/classes/marineland/entities/onboard/OnboardEntity.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/soyouz/classes/marineland/entities/onboard/OnboardEntity.java
@@ -1,11 +1,13 @@
 package fr.unice.polytech.si3.qgl.soyouz.classes.marineland.entities.onboard;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import fr.unice.polytech.si3.qgl.soyouz.classes.marineland.entities.Entity;
 
 /**
  * Super Class for every entities on the deck.
  */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", defaultImpl = Void.class)
 @JsonSubTypes({
     @JsonSubTypes.Type(value = Rame.class, name = "oar")
 })
