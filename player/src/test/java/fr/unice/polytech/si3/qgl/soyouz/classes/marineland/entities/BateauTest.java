4612f6169a4c127186ca6597ce30d9ceb33f9403
Eschimese Alexis <alexis.eschimese@etu.unice.fr>
2021-01-25 20:01:35 +0100

    Adding Tests 1.85/2, more comming later

--- player/src/test/java/fr/unice/polytech/si3/qgl/soyouz/classes/marineland/entities/BateauTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/soyouz/classes/marineland/entities/BateauTest.java
@@ -1,10 +1,13 @@
 package fr.unice.polytech.si3.qgl.soyouz.classes.marineland.entities;
 
 import fr.unice.polytech.si3.qgl.soyouz.Cockpit;
-import fr.unice.polytech.si3.qgl.soyouz.classes.marineland.Deck;
+import fr.unice.polytech.si3.qgl.soyouz.classes.marineland.entities.onboard.OnboardEntity;
 import fr.unice.polytech.si3.qgl.soyouz.classes.marineland.entities.onboard.Rame;
+import fr.unice.polytech.si3.qgl.soyouz.classes.utilities.Pair;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -91,9 +94,15 @@ class BateauTest {
 
     @Test
     void getEntityHere() {
+        assertTrue(ship.getEntityHere(0, 0).isPresent());
+        assertTrue(ship.getEntityHere(1, 0).isPresent());
+        assertTrue(ship.getEntityHere(1, 1).isEmpty());
     }
 
     @Test
     void testGetEntityHere() {
+        assertTrue(ship.getEntityHere(Pair.of(0, 0)).isPresent());
+        assertTrue(ship.getEntityHere(Pair.of(1, 0)).isPresent());
+        assertTrue(ship.getEntityHere(Pair.of(1, 1)).isEmpty());
     }
 }
