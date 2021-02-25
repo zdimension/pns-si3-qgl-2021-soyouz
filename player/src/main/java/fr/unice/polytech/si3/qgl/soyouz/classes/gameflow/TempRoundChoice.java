27973a6e1e7011a0ca98efb8a125955fea4cb412
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2021-02-25 17:25:29 +0100

    update class #8

--- player/src/main/java/fr/unice/polytech/si3/qgl/soyouz/classes/gameflow/TempRoundChoice.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/soyouz/classes/gameflow/TempRoundChoice.java
@@ -16,123 +16,186 @@ import java.util.stream.Stream;
  * Class storing intended action for the next round.
  */
 public class TempRoundChoice {
-    final HashMap<Marin, Pair<PosOnShip, Pair<GameAction, @Nullable GameAction>>> sailorsJob;
-    final Collection<Marin> vacantSailors;
-    final HashMap<OnboardEntity, Boolean> usedOnBoardEntity;
+	final HashMap<Marin, Pair<PosOnShip, Pair<GameAction, @Nullable GameAction>>> sailorsJob;
+	final HashMap<Marin, Pair<PosOnShip, MoveAction>> movedSailors;
+	final Collection<Marin> vacantSailors;
+	final HashMap<OnboardEntity, Boolean> usedOnBoardEntity;
 
-    /**
-     * Constructor.
-     *
-     * @param entity All entities on board.
-     * @param sailors All sailors on board.
-     */
-    public TempRoundChoice(Collection<OnboardEntity> entity, Collection<Marin> sailors){
-        sailorsJob = new HashMap<>();
-        usedOnBoardEntity = new HashMap<>();
-        vacantSailors = sailors;
-        for(var ent : entity){
-            usedOnBoardEntity.put(ent, false);
-        }
-    }
+	/**
+	 * Constructor.
+	 *
+	 * @param entity  All entities on board.
+	 * @param sailors All sailors on board.
+	 */
+	public TempRoundChoice(Collection<OnboardEntity> entity, Collection<Marin> sailors) {
+		sailorsJob = new HashMap<>();
+		usedOnBoardEntity = new HashMap<>();
+		movedSailors = new HashMap<>();
+		vacantSailors = sailors;
+		for (var ent : entity) {
+			usedOnBoardEntity.put(ent, false);
+		}
+	}
 
-    /**
-     * Getters;
-     *
-     * @return all sailors jobs.
-     */
-    public Map<Marin, Pair<PosOnShip, Pair<GameAction, @Nullable GameAction>>> getSailorsJob() {
-        return Collections.unmodifiableMap(sailorsJob);
-    }
+	/**
+	 * Getters;
+	 *
+	 * @return all sailors jobs.
+	 */
+	public Map<Marin, Pair<PosOnShip, Pair<GameAction, @Nullable GameAction>>> getSailorsJob() {
+		return Collections.unmodifiableMap(sailorsJob);
+	}
 
-    /**
-     * Getters.
-     *
-     * @return all on board entities that are used.
-     */
-    public Map<OnboardEntity, Boolean> getUsedOnBoardEntity() {
-        return Collections.unmodifiableMap(usedOnBoardEntity);
-    }
+	/**
+	 * Getters.
+	 *
+	 * @return all on board entities that are used.
+	 */
+	public Map<OnboardEntity, Boolean> getUsedOnBoardEntity() {
+		return Collections.unmodifiableMap(usedOnBoardEntity);
+	}
 
-    /**
-     * Getters.
-     *
-     * @return all vacant sailors.
-     */
-    public Collection<Marin> getVacantSailors() {
-        return Collections.unmodifiableCollection(vacantSailors);
-    }
+	/**
+	 * Getters.
+	 *
+	 * @return all vacant sailors.
+	 */
+	public Collection<Marin> getUnmovedVacantSailors() {
+		return Collections.unmodifiableCollection(vacantSailors);
+	}
 
-    public Collection<Marin> getBusySailors() { return Collections.unmodifiableCollection(sailorsJob.keySet());}
+	public Collection<Marin> getMovedVacantSailors() {
+		return Collections.unmodifiableCollection(movedSailors.keySet());
+	}
 
-    public Set<GameAction> getAllActions() {
-        return sailorsJob.values().stream().
-                map(Pair::getSecond).
-                flatMap(p -> Stream.of(p.getFirst(), p.getSecond())).
-                filter(Objects::nonNull).
-                collect(Collectors.toSet());
-    }
+	public Collection<Marin> getBusySailors() {
+		return Collections.unmodifiableCollection(sailorsJob.keySet());
+	}
 
-    public Set<MoveAction> getAllMoves(){
-        return getAllActions().stream().filter(a -> a instanceof MoveAction).map(a -> (MoveAction)a).collect(Collectors.toSet());
-    }
+	public Set<GameAction> getAllActions() {
+		return new HashSet<>() {{
+			addAll(sailorsJob.values().stream().
+					map(Pair::getSecond).
+					flatMap(p -> Stream.of(p.getFirst(), p.getSecond())).
+					filter(Objects::nonNull).
+					collect(Collectors.toSet()));
+			addAll(movedSailors.values().stream().
+					map(Pair::getSecond).
+					collect(Collectors.toSet())
+			);
+		}};
+	}
 
-    /**
-     * Making a sailor perform an action and updating onBoardEntity usage.
-     *
-     * @param sailor to perform action.
-     * @param pos new position of the sailor.
-     * @param action for sailor to perform (anyAction, null) (moveAction, null) or (moveAction, AnyAction).
-     * @throws IllegalArgumentException when illegal arguments.
-     */
-    public void hireSailor(Marin sailor, Pair<Integer,Integer> pos, Pair<GameAction, @Nullable GameAction> action) throws IllegalArgumentException{
-        if(sailorsJob.containsKey(sailor))
-            throw new IllegalArgumentException("Sailor not vacant");
+	public Set<MoveAction> getAllMoves() {
+		return getAllActions().stream().filter(a -> a instanceof MoveAction).map(a -> (MoveAction) a).collect(Collectors.toSet());
+	}
 
-        var optEntity = usedOnBoardEntity.keySet().stream().filter(e -> e.getPos() == pos).findAny();
-        if(optEntity.isEmpty())
-            throw new IllegalArgumentException("Entity no found");
+	//todo check if sailor can perform action HERE
 
-        var entity = optEntity.get();
-        if(usedOnBoardEntity.get(entity))
-            throw new IllegalArgumentException("Entity already used");
+	/**
+	 * Making a sailor perform an action and updating onBoardEntity usage.
+	 *
+	 * @param sailor to perform action.
+	 * @param pos    new position of the sailor.
+	 * @param action for sailor to perform (anyAction, null) (moveAction, null) or (moveAction, AnyAction).
+	 * @throws IllegalArgumentException when illegal arguments.
+	 */
+	public void hireSailor(Marin sailor, Pair<Integer, Integer> pos, Pair<GameAction, @Nullable GameAction> action) {
+		if (sailorsJob.containsKey(sailor))
+			throw new IllegalArgumentException("Sailor not vacant");
 
-        if(!vacantSailors.contains(sailor))
-            throw new IllegalArgumentException("Sailor not found");
+		OnboardEntity entity = null;
+		if (action.getSecond() != null) {
+			var optEntity = usedOnBoardEntity.keySet().stream().filter(e -> e.getPos() == pos).findAny();
+			if (optEntity.isEmpty())
+				throw new IllegalArgumentException("Entity no found");
 
+			entity = optEntity.get();
+			if (usedOnBoardEntity.get(entity))
+				throw new IllegalArgumentException("Entity already used");
 
-        if(!sailor.getPos().equals(pos)){
-            var isActionMove = action != null && action.first instanceof MoveAction;
+			if (action.getSecond().getEntityNeeded().isInstance(entity))
+				throw new IllegalArgumentException("Entity not compatible with action");
+		}
 
-            if(!isActionMove){
-                throw new IllegalArgumentException("Incoherent sailor position (position changed but no moveAction)");
-            }
+		if (sailorsJob.containsKey(sailor))
+			throw new IllegalArgumentException("Sailor not found");
 
-            var actionMove = (MoveAction)action.first;
+		if (vacantSailors.contains(sailor)) {
 
-            if(!actionMove.newPos().equals(pos)){
-                throw new IllegalArgumentException("Incoherent sailor position (position not coherent with moveAction)");
-            }
-        }
+			if (!sailor.getPos().equals(pos)) {
+				var isActionMove = action != null && action.first instanceof MoveAction;
 
-        vacantSailors.remove(sailor);
-        sailorsJob.put(sailor, Pair.of(new PosOnShip(pos.getFirst(), pos.getSecond()), action));
-        usedOnBoardEntity.replace(entity, true);
-    }
+				if (!isActionMove)
+					throw new IllegalArgumentException("Incoherent sailor position (position changed but no moveAction)");
 
-    /**
-     * Making a sailor perform an action and updating onBoardEntity usage
-     *
-     * @param sailor to perform action
-     * @param pos  new position of the sailor
-     * @param act1 for sailor to perform
-     * @param act2 for sailor to perform (only if the first action is MoveAction
-     * @throws IllegalArgumentException when illegal arguments
-     */
-    public void hireSailor(Marin sailor, Pair<Integer,Integer> pos, GameAction act1, @Nullable GameAction act2) throws IllegalArgumentException{
-        hireSailor(sailor, pos, Pair.of(act1, act2));
-    }
+				var actionMove = (MoveAction) action.first;
 
-    public void hireSailor(Marin sailor, PosOnShip pos, GameAction act1, @Nullable GameAction act2) throws IllegalArgumentException{
-        hireSailor(sailor, pos.getPos(), Pair.of(act1, act2));
-    }
+				if (!actionMove.newPos().equals(pos))
+					throw new IllegalArgumentException("Incoherent sailor position (position not coherent with moveAction)");
+			}
+			vacantSailors.remove(sailor);
+			if (entity != null) {
+				usedOnBoardEntity.replace(entity, true);
+				sailorsJob.put(sailor, Pair.of(new PosOnShip(pos.getFirst(), pos.getSecond()), action));
+			} else {
+				movedSailors.put(sailor, Pair.of(new PosOnShip(pos.getFirst(), pos.getSecond()), (MoveAction) action.getFirst()));
+			}
+		} else if (movedSailors.containsKey(sailor)) {
+			if (action.getSecond() != null)
+				throw new IllegalArgumentException("Sailor cannot perform 2 extra actions");
+			if (action.getFirst() instanceof MoveAction)
+				throw new IllegalArgumentException("Sailor already moved");
+
+			var oldSailor = movedSailors.remove(sailor);
+			sailorsJob.put(sailor, Pair.of(new PosOnShip(pos.getFirst(), pos.getSecond()), Pair.of(oldSailor.getSecond(), action.getFirst())));
+			usedOnBoardEntity.replace(entity, true);
+		} else {
+			throw new IllegalArgumentException("Sailor not found");
+		}
+	}
+
+	/**
+	 * Making a sailor perform an action and updating onBoardEntity usage
+	 *
+	 * @param sailor to perform action
+	 * @param pos    new position of the sailor
+	 * @param act1   for sailor to perform
+	 * @param act2   for sailor to perform (only if the first action is MoveAction
+	 * @throws IllegalArgumentException when illegal arguments
+	 */
+	public void hireSailor(Marin sailor, Pair<Integer, Integer> pos, GameAction act1, @Nullable GameAction act2) {
+		hireSailor(sailor, pos, Pair.of(act1, act2));
+	}
+
+	public void hireSailor(Marin sailor, PosOnShip pos, GameAction act1, @Nullable GameAction act2) {
+		hireSailor(sailor, pos.getPosCoord(), Pair.of(act1, act2));
+	}
+
+	public void hireSailor(Marin sailor, GameAction act1) {
+		if (act1 instanceof MoveAction) {
+			hireSailor(sailor, (MoveAction) act1);
+			return;
+		}
+		if (vacantSailors.contains(sailor)) {
+			hireSailor(sailor, sailor.getPos(), Pair.of(act1, null));
+		} else if (movedSailors.containsKey(sailor)) {
+			var mov = movedSailors.get(sailor).first;
+			hireSailor(sailor, mov.getPosCoord(), Pair.of(act1, null));
+		} else
+			throw new IllegalArgumentException("Cannot hire sailor");
+	}
+
+	public void moveSailor(MoveAction move) {
+		hireSailor(move.getSailor(), Pair.of(move.newPos().first, move.newPos().getSecond()), Pair.of(move, null));
+	}
+
+	public Marin findFirstVacantSailorHere(Pair<Integer, Integer> pos) {
+		var unmoved = vacantSailors.stream().filter(s -> s.getPos().equals(pos)).findFirst();
+		if (unmoved.isPresent()) {
+			return unmoved.get();
+		}
+		var moved = movedSailors.entrySet().stream().filter(s -> s.getValue().first.getPosCoord().equals(pos)).findFirst();
+		return moved.map(Map.Entry::getKey).orElse(null);
+	}
 }
