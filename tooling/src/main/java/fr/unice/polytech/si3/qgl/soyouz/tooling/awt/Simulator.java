de1f4cf4df6eb2687b7068d3f37bc39e65daed01
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2021-03-14 14:32:37 +0100

    Experimental wind support

--- tooling/src/main/java/fr/unice/polytech/si3/qgl/soyouz/tooling/awt/Simulator.java
+++ tooling/src/main/java/fr/unice/polytech/si3/qgl/soyouz/tooling/awt/Simulator.java
@@ -18,8 +18,10 @@ import fr.unice.polytech.si3.qgl.soyouz.classes.marineland.Marin;
 import fr.unice.polytech.si3.qgl.soyouz.classes.marineland.entities.Entity;
 import fr.unice.polytech.si3.qgl.soyouz.classes.marineland.entities.onboard.OnboardEntity;
 import fr.unice.polytech.si3.qgl.soyouz.classes.marineland.entities.onboard.Rame;
+import fr.unice.polytech.si3.qgl.soyouz.classes.marineland.entities.onboard.Voile;
 import fr.unice.polytech.si3.qgl.soyouz.classes.parameters.InitGameParameters;
 import fr.unice.polytech.si3.qgl.soyouz.classes.parameters.NextRoundParameters;
+import fr.unice.polytech.si3.qgl.soyouz.classes.utilities.Util;
 
 import javax.swing.*;
 import java.awt.*;
@@ -31,6 +33,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Simulator extends JFrame
@@ -42,6 +45,7 @@ public class Simulator extends JFrame
     private int currentStep = 0;
     private double rotIncrement;
     private double spdIncrement;
+    private NextRoundParameters np;
 
     public Simulator() throws IOException
     {
@@ -125,10 +129,20 @@ public class Simulator extends JFrame
             @Override
             public void actionPerformed(ActionEvent event)
             {
+                var sails = Util.filterType(Arrays.stream(np.getShip().getEntities()), Voile.class);
+                var counts = new Object(){int open; int total;};
+                sails.forEach(voile ->
+                {
+                    if (voile.isOpenned())
+                        counts.open++;
+                    counts.total++;
+                });
+                var windFac = np.getWind().windAdditionnalSpeed(counts.total, counts.open, np.getShip()) / COMP_STEPS;
+                var linSpeed = spdIncrement + windFac;
                 var cur = model.getShip().getPosition();
                 model.getShip().setPosition(cur.add(new Position(
-                    spdIncrement * Math.cos(cur.getOrientation()),
-                    spdIncrement * Math.sin(cur.getOrientation()),
+                    linSpeed * Math.cos(cur.getOrientation()),
+                    linSpeed * Math.sin(cur.getOrientation()),
                     rotIncrement)));
                 System.out.println("Ship position : " + model.getShip().getPosition());
                 if (++currentStep >= COMP_STEPS)
@@ -143,7 +157,8 @@ public class Simulator extends JFrame
         btnNext.addActionListener(event ->
         {
             btnNext.setEnabled(false);
-            var np = new NextRoundParameters(model.getShip(), null, new Entity[0]); //TODO J'ai
+            //TODO J'ai
+            np = new NextRoundParameters(model.getShip(), null, new Entity[0]);
             // mis null a la place du vent
             try
             {
