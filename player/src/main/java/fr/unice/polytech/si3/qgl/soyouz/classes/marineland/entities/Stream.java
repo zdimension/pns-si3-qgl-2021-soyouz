db84c6f4d6537b3b1c9466d0b1a56a7eb7fe2b4e
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2021-03-25 12:50:58 +0100

    Compute Stream projected strength

--- player/src/main/java/fr/unice/polytech/si3/qgl/soyouz/classes/marineland/entities/Stream.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/soyouz/classes/marineland/entities/Stream.java
@@ -1,5 +1,7 @@
 package fr.unice.polytech.si3.qgl.soyouz.classes.marineland.entities;
 
+import fr.unice.polytech.si3.qgl.soyouz.classes.geometry.Position;
+
 public class Stream extends ShapedEntity implements Entity
 {
     private double strength;
@@ -7,5 +9,11 @@ public class Stream extends ShapedEntity implements Entity
     public double getStrength()
     {
         return strength;
+    }
+
+    public Position getProjectedStrength()
+    {
+        var angle = getPosition().getOrientation();
+        return new Position(strength * Math.cos(angle), strength * Math.sin(angle), 0);
     }
 }
