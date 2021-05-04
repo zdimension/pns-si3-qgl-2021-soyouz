10d3e6e1d9846788af66e8a8aba5a8a33ad46b7e
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2021-05-04 23:26:56 +0200

    Add Week12, add support for more than 2 multiplayer JARs

--- generate_duplicate_player.bat
+++ generate_duplicate_player.bat
@@ -1,2 +1,6 @@
 @echo off
-java -jar jarjar.jar --rules rules.txt player/target/soyouz-0.1-SNAPSHOT.jar --output player/target/soyouz2-0.1-SNAPSHOT.jar
+
+for /l %%x in (2, 1, 5) do (
+	echo rule fr.unice.polytech.si3.qgl.soyouz.** fr.unice.polytech.si3.qgl.soyouz%%x.@1> rules.txt
+	java -jar jarjar.jar --rules rules.txt player/target/soyouz-0.1-SNAPSHOT.jar --output player/target/soyouz%%x-0.1-SNAPSHOT.jar
+)
